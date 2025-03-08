153d0a7666c109c85dcdd2efc124b332b0e778a9
sujtha <sujithaselvi18@gmail.com>
2025-03-08 11:42:13 +0530

    Initial commit with README and Main.java

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+			
+		//1. Launch browser
+		ChromeDriver driver = new ChromeDriver(); // pre-defined class
+
+		
+		//2. Open website
+		driver.get("https://demo.opencart.com/");
+		//example website
+//		driver.get("http://127.0.0.1:5500/homepage.html");
+		
+	    //3. Fetch the title
+		String actualTitle = driver.getTitle();
+		
+		//4. Compare expected and actual(fetched) title
+		//5. Display the result
+		String expectedTitle = "Your Store";
+		System.out.println("Expected Title : " + expectedTitle);
+		System.out.println("Actual Title : " + actualTitle);
+		
+		//example website
+//		String expectedTitle = "KFC";
+//		System.out.println("Expected Title : " + expectedTitle);
+//		System.out.println("Actual Title : " + actualTitle);
+		
+		if(expectedTitle.contentEquals(actualTitle))
+		{
+			System.out.println("Title verification passed");
+		}
+		else
+		{
+			System.out.println("Title verification failed");
+		}
+
+		//6. Close browser
+		Thread.sleep(5000); // website will close within 5 seconds after opening
+		driver.close();
+		
+		
+
+	}
+
+}
